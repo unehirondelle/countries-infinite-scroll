4ba8fc92da96170a27ea94d4c35771e199dab395
Irina Plaksina <iplaksina@vigilantioe.com>
2021-12-02 00:44:21 -0500

    got d20 countries per click

--- app/screens/CountriesList.js
+++ app/screens/CountriesList.js
@@ -1,6 +1,6 @@
-import React, {useEffect, useState} from 'react';
+import React, {Fragment, useEffect, useState} from 'react';
 import {createServer} from 'miragejs';
-import {View, Text} from 'react-native';
+import {View, Text, Button} from 'react-native';
 import countriesData from '../data/countries';
 
 if (window.server) {
@@ -9,30 +9,38 @@ if (window.server) {
 
 window.server = createServer({
     routes() {
-        this.get('/api/countries', () => {
-            return countriesData;
-        })
-    },
+        this.get(`/api/countries`, (schema, request) => {
+            const {offset, limit} = request.queryParams;
+            return countriesData.slice(offset, parseInt(offset) + parseInt(limit));
+        });
+    }
 });
 
 const CountriesList = () => {
     const [countries, setCountries] = useState([]);
+    const [offsetNumber, setOffsetNumber] = useState(0);
 
     useEffect(() => {
-        fetch('/api/countries')
+        fetch(`/api/countries?offset=${offsetNumber}&limit=20`)
             .then(res => res.json())
-            .then(json => setCountries(json))
+            .then(json => setCountries(current => current.concat(json)))
             .catch(err => console.log(`/api/countries error ${err}`));
-    }, []);
+    }, [offsetNumber]);
 
     return (
-        <View style={{backgroundColor: 'grey'}}>
-            {countries.map(({name}) => (
-                <Text key={name}>
-                    {name}
-                </Text>
-            ))}
-        </View>
+        <Fragment>
+            <View style={{backgroundColor: 'grey'}}>
+                {countries.map(({name}) => (
+                    <Text key={name}>
+                        {name}
+                    </Text>
+                ))}
+            </View>
+
+            <View>
+                <Button title={'More'} onPress={() => setOffsetNumber(offsetNumber + 20)}/>
+            </View>
+        </Fragment>
     );
 };
 
